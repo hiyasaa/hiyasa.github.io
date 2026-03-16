786391d581a58969ba0f66b711911be93eb391f9
hiyasaa <fathimahiyasaa@gmail.com>
2026-03-16 12:29:21 +0530

    Refactor type function for improved readability

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let currentText = "";
 let letter = "";
 
 function type(){
-if(count === text.length){count=0;}
-currentText = text[count];
-letter = currentText.slice(0, ++index);
-document.querySelector(".typing").textContent = letter;
-if(letter.length === currentText.length){
-count++;
-index=0;
-setTimeout(type,1000);
-}else{
-setTimeout(type,100);
-}
+  if(count === text.length){count=0;}
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+  document.querySelector(".typing").textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index=0;
+    setTimeout(type,1000);
+  }else{
+    setTimeout(type,100);
+  }
 }
 
 type();
