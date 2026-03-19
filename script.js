e5ed29e81f5a345bf2914adca18e51f8cf99a090
hiyasaa <fathimahiyasaa@gmail.com>
2026-03-19 12:28:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-.slider {
-    width: 100%;
-    padding: 10px;
-}
-
 .slider img {
     width: 100%;
     height: 450px;
     object-fit: cover;
     border-radius: 20px;
-    display: block;
-    image-rendering: auto;
+    transform: scale(1.01);
 }
