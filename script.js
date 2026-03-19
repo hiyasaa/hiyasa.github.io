d4cc8eada97b69db847fdd0cc068b4a7cd23e9b2
hiyasaa <fathimahiyasaa@gmail.com>
2026-03-19 12:15:15 +0530

    Add image slideshow functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+<script>
+let images = [
+    "images/abaya1.jpg",
+    "images/abaya2.jpg",
+    "images/abaya3.jpg",
+    "images/abaya4.jpg"
+];
 
+let i = 0;
+
+setInterval(() => {
+    i = (i + 1) % images.length;
+    document.getElementById("slide").src = images[i];
+}, 3000);
+</script>
